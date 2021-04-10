f31421e4d29cef295567a9f6a93abbb51d0df2d5
Gramli <Gramli@seznam.cz>
2021-04-10 16:44:11 +0200

    Finish Implementation of local notifications
    Fix app icon

--- lib/service/local_notification_service.dart
+++ lib/service/local_notification_service.dart
@@ -26,7 +26,6 @@ class LocalNotificationService {
 
   Future<void> _init() async {
     _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-// initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
     const AndroidInitializationSettings initializationSettingsAndroid =
         AndroidInitializationSettings('@mipmap/ic_launcher');
     final IOSInitializationSettings initializationSettingsIOS =
